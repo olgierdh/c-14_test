be2b34075ca503ab6ae9a7696a06cb8e077c586b
Olgierd Humenczuk <olgierd.humenczuk@tieto.com>
2017-09-19 07:44:19 +0200

    Changed spaces to tabs

--- main.cxx
+++ main.cxx
@@ -2,51 +2,51 @@
 
 template < typename T, int N > struct tag
 {
-    constexpr friend auto loophole( tag< T, N > );
+	constexpr friend auto loophole( tag< T, N > );
 };
 
 template < typename T, typename U, int N > struct fnc_def
 {
-    constexpr friend auto loophole( tag< T, N > )
-    {
-        return U{};
-    }
+	constexpr friend auto loophole( tag< T, N > )
+	{
+		return U{};
+	}
 };
 
 template < typename T, int N > struct c_op
 {
-    template < typename U, typename = decltype( fnc_def< T, U, N >{}, tag< T, N >{} ) >
-    operator U();
+	template < typename U, typename = decltype( fnc_def< T, U, N >{}, tag< T, N >{} ) >
+	operator U();
 };
 
 template < typename T > struct m_tag
 {
-    constexpr friend auto get_m( m_tag< T > );
+	constexpr friend auto get_m( m_tag< T > );
 };
 
 template < typename T, typename N > struct map_impl
 {
-    constexpr friend auto get_m( m_tag< T > )
-    {
-        return N{};
-    }
+	constexpr friend auto get_m( m_tag< T > )
+	{
+		return N{};
+	}
 };
 
 template < typename First, typename Second > struct m_pair
 {
-    using first_type  = First;
-    using second_type = Second;
+	using first_type  = First;
+	using second_type = Second;
 };
 
 template < typename T, typename N, typename = decltype( map_impl< T, N >{} ) >
 constexpr int register_type_f( m_tag< T > )
 {
-    return 0;
+	return 0;
 }
 
 template < typename T, typename N >
 using register_type = decltype( register_type_f< T, N >( m_tag< T >{} ),
-                                register_type_f< N, T >( m_tag< N >{} ) );
+								register_type_f< N, T >( m_tag< N >{} ) );
 
 template < typename... Ts > struct sequence
 {
@@ -54,8 +54,8 @@ template < typename... Ts > struct sequence
 
 template < typename... P > struct make_map_impl
 {
-    using value_type =
-        sequence< register_type< typename P::first_type, typename P::second_type >... >;
+	using value_type =
+		sequence< register_type< typename P::first_type, typename P::second_type >... >;
 };
 
 template < typename... Ts >
@@ -63,8 +63,8 @@ using register_types = typename make_map_impl< Ts... >::value_type;
 
 struct foo
 {
-    int a;
-    char b;
+	int a;
+	char b;
 };
 
 struct bar
@@ -73,15 +73,15 @@ struct bar
 
 int main( int argc, const char* argv[] )
 {
-    using t0 = register_types< m_pair< foo, char >, m_pair< bar, int > >;
+	using t0 = register_types< m_pair< foo, char >, m_pair< bar, int > >;
 
-    static_assert( std::is_same< decltype( get_m( m_tag< foo >{} ) ), char >::value, "" );
-    static_assert( std::is_same< decltype( get_m( m_tag< bar >{} ) ), int >::value, "" );
-    static_assert( std::is_same< decltype( get_m( m_tag< int >{} ) ), bar >::value, "" );
-    static_assert( std::is_same< decltype( get_m( m_tag< char >{} ) ), foo >::value, "" );
+	static_assert( std::is_same< decltype( get_m( m_tag< foo >{} ) ), char >::value, "" );
+	static_assert( std::is_same< decltype( get_m( m_tag< bar >{} ) ), int >::value, "" );
+	static_assert( std::is_same< decltype( get_m( m_tag< int >{} ) ), bar >::value, "" );
+	static_assert( std::is_same< decltype( get_m( m_tag< char >{} ) ), foo >::value, "" );
 
-    using vt = decltype( foo{c_op< foo, 0 >{}, c_op< foo, 1 >{}} );
-    static_assert( std::is_same< decltype( loophole( tag< foo, 0 >{} ) ), int >::value,
-                   "" );
-    return 0;
+	using vt = decltype( foo{c_op< foo, 0 >{}, c_op< foo, 1 >{}} );
+	static_assert( std::is_same< decltype( loophole( tag< foo, 0 >{} ) ), int >::value,
+				   "" );
+	return 0;
 }
